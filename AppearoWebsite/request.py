ebcbb0e7580a8e7fe1c15d9132b66fbb5c067b69
abender14 <bender.a@husky.neu.edu>
2020-04-23 17:47:30 -0400

    Website re-org

---
+++ AppearoWebsite/request.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 20 23:15:40 2020
+
+@author: alexb
+"""
+
+import requests
+
+url = 'http://localhost:5000/predict_api'
+r = requests.post(url,json={'RevolvingUtilizationOfUnsecuredLines':1.1,
+                            'age':30,
+                            'NumberOfDependents':1,
+                            'DebtRatio':1.5,
+                            'MonthlyIncome':3000.0,
+                            'NumberRealEstateLoansOrLines':1,
+                            'NumberOfOpenCreditLinesAndLoans':1,
+                            'NumberOfTime30-59DaysPastDueNotWorse':1,
+                            'NumberOfTime60-89DaysPastDueNotWorse':1,
+                            'NumberOfTimes90DaysLate':1})
+
+print(r.json())
